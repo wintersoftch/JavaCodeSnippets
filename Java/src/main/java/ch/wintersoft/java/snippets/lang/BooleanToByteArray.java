a238162c8ee261070452951df45f15e87aeeef34
Tom Winterhalder <tom.winterhalder@gmail.com>
2025-12-21 11:03:49 +0100

    enabled sonar (#8)

--- Java/src/main/java/ch/wintersoft/java/snippets/lang/BooleanToByteArray.java
+++ Java/src/main/java/ch/wintersoft/java/snippets/lang/BooleanToByteArray.java
@@ -15,7 +15,7 @@ public class BooleanToByteArray {
    *   <li>convertBooleansToByteArray(false, true)=byte[]{2}
    * </ul>
    *
-   * @param booleans boolean values to be converted to byte[]
+   * @param booleans a vararg sequence of boolean values to be converted to byte[]
    * @return Array of bytes representing the received boolean values
    */
   public byte[] convertBooleansToByteArray(boolean... booleans) {
@@ -23,7 +23,10 @@ public class BooleanToByteArray {
     final double bitCount = 8;
     ByteArrayOutputStream bout = new ByteArrayOutputStream();
     for (int i = 0; i < booleans.length; i++) {
+      // Mask with 0xFF to treat byte as unsigned during bitwise OR, then shift the bit into
+      // position
       aByte = (byte) ((aByte & 0xFF) | (booleans[i] ? 1 : 0) << (int) (i % bitCount));
+      // Flush to stream if byte is full or if we've reached the end of the input
       if ((i > 0 && (i + 1) % bitCount == 0) || i == booleans.length - 1) {
         bout.write(aByte);
         aByte = 0;
